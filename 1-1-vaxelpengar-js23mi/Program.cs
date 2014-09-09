23b49d4b4f90fa13039285016af92d44ead8f4d5
Jaroslaw Szymczak <admin@webb365.se>
2014-09-09 09:23:07 +0200

    Lagt till en del av metoderna

---
+++ 1-1-vaxelpengar-js23mi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1_vaxelpengar_js23mi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static double ReadPositveDoble(string prompt){
+
+            return 5.34;
+        }
+
+        private static uint ReadUint(string prompt, uint minValue)
+        {
+           
+            return 99;
+        }
+
+        private static uint[] SplitIntoDenominations(uint change, uint[] denominations)
+        {
+            denominations= new uint[]{55,76};
+
+            return denominations;
+        } 
+    
+    }
+}
