72817324513ae2bb50ff9440e9e0462d19ef9c5d
Jaroslaw Szymczak <admin@webb365.se>
2014-09-09 14:42:03 +0200

    Gjort klar ReadPositiveDouble

--- 1-1-vaxelpengar-js23mi/Program.cs
+++ 1-1-vaxelpengar-js23mi/Program.cs
@@ -6,21 +6,33 @@ using System.Threading.Tasks;
 
 namespace _1_1_vaxelpengar_js23mi{
     class Program{
-        static void Main(string[] args){
+         static void Main(string[] args){
               do {
-                  ReadPositiveDouble(Properties.Resources.Total_sum);
+                 Console.WriteLine( ReadPositiveDouble(Properties.Resources.Total_sum));
               } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
 
         private static double ReadPositiveDouble(string prompt){
-            double sum = 6.76;
-            Console.Write(prompt);
-            sum = double.Parse(Console.ReadLine());
-            return sum;
+            double sum;
+            while (true) {
+                Console.Write(prompt);
+                try{
+                    sum = Math.Round(double.Parse(Console.ReadLine()));
+                    if (sum < 1){
+                        throw new Exception();
+                    }
+                    return sum;
+                }
+                catch {
+                    // använd  viewmessage när det blir klart
+                    Console.WriteLine("FEL");
+                } 
+            }
+
         }
 
         private static uint ReadUint(string prompt, uint minValue){
-           
+
             return 99;
         }
 
@@ -31,7 +43,7 @@ namespace _1_1_vaxelpengar_js23mi{
         }
 
         private static void ViewMessage(string message,bool isError = false) {
-        
+
         }
 
         private static void ViewReceipt(double subtotal, double roundingOffAmount, uint total, uint change, uint[] notes, uint[] denominations){
