ca83b1672893a590c0b694f89998b198d8658cd6
Jaroslaw Szymczak <admin@webb365.se>
2014-09-09 09:29:00 +0200

    Alla metoder är klara

--- 1-1-vaxelpengar-js23mi/Program.cs
+++ 1-1-vaxelpengar-js23mi/Program.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1_1_vaxelpengar_js23mi
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace _1_1_vaxelpengar_js23mi{
+    class Program{
+        static void Main(string[] args){
 
         }
 
@@ -18,18 +15,24 @@ namespace _1_1_vaxelpengar_js23mi
             return 5.34;
         }
 
-        private static uint ReadUint(string prompt, uint minValue)
-        {
+        private static uint ReadUint(string prompt, uint minValue){
            
             return 99;
         }
 
-        private static uint[] SplitIntoDenominations(uint change, uint[] denominations)
-        {
+        private static uint[] SplitIntoDenominations(uint change, uint[] denominations){
             denominations= new uint[]{55,76};
 
             return denominations;
-        } 
+        }
+
+        private static void ViewMessage(string message,bool isError = false) {
+        
+        }
+
+        private static void ViewReceipt(double subtotal, double roundingOffAmount, uint total, uint change, uint[] notes, uint[] denominations){
+
+        }
     
     }
 }
